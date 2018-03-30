a6debabca6902b12a3e54aaa1b6bdfdf9c1167c4
jmcph4 <jmcph4@users.noreply.github.com>
2018-03-30 18:34:54 +1000

    Add home page
    This commit adds an index page for the entire site.

--- pytrade/urls.py
+++ pytrade/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin, auth
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
     path('trade/', include('trade.urls')),
     path('accounts/', include('django.contrib.auth.urls'))
